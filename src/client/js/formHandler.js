5faa6904294fa9c7b34e873e28228d59ca8e21a0
sparsh <sparshgr8@yahoo.com>
2021-12-19 23:08:13 +0530

    Successful API connection

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,14 +4,17 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('name').value
 
-    console.log("::: Form Submitted :::")
     fetch(`http://localhost:8080/test?text=${formText}`)
     .then(res => res.json())
     .then(function(res) {
         const text = 
-        `Aggreement: ${res.agreement} <br>
-         Subjectivity: ${res.subjectivity} <br>
-         Confidence: ${res.confidence}`
+        `
+         Model: ${res.model}<br>
+         Score Tag: ${res.score_tag}<br>
+         Agreement: ${res.agreement}<br>
+         Subjectivity: ${res.subjectivity}<br>
+         Confidence: ${res.confidence}<br>
+         Irony: ${res.irony}`
         document.getElementById('results').innerHTML = text
     })
 }
