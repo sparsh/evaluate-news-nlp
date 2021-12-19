dd41540c369e4266b9fe44429722039916189b97
sparsh <sparshgr8@yahoo.com>
2021-12-19 20:38:44 +0530

    Code changes for webpack setup in local and prod

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -5,7 +5,8 @@ function checkForName(inputText) {
         "Janeway",
         "Kirk",
         "Archer",
-        "Georgiou"
+        "Georgiou",
+        "sparsh"
     ]
 
     if(names.includes(inputText)) {
