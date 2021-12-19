809bc6a856882517ab8bd47741888fb6b26ea2a0
sparsh <sparshgr8@yahoo.com>
2021-12-19 20:47:21 +0530

    Dependencies updated to latest version

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require("path");
 const webpack = require("webpack");
 module.exports = {
@@ -50,6 +50,6 @@ module.exports = {
       protectWebpackAssets: false
   }),
   new MiniCssExtractPlugin({ filename: "[name].css" }),
-  new WorkboxPlugin.GenerateSW()
+  // new WorkboxPlugin.GenerateSW()
   ]
 };
