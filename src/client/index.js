ba19b3e709670cfadbd40abde9e08923de015694
sparsh <sparshgr8@yahoo.com>
2021-12-19 20:50:24 +0530

    Added css in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,8 @@
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
